7630d491b9309a9304204d5106478239c028e4b5
Web3Alliance <official@theweb3alliance.org>
2026-03-28 16:39:41 +0100

    Fix toaster hydration error

--- components/toaster-client.tsx
+++ components/toaster-client.tsx
@@ -1,6 +1,16 @@
 'use client'
+
+import { useEffect, useState } from 'react'
 import { Toaster } from '@/components/ui/toaster'
 
 export function ToasterClient() {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
+
   return <Toaster />
 }
